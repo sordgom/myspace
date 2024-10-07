1c2cc450823a02e31e53d4a4c0f896f832a9afa5
sordgom <aymaneigmiden@gmail.com>
2024-10-07 21:11:33 +0800

    Project setup

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. This is where I spend most of my time. I work here, play video games, surf the internet, and build fun projects like this one.
+    Here is my <a href="https://github.com/sordgom" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Youtube or Twitch. It hasn't since a lot of action (•́︵•̀)`,
+  tv: `This's my TV. I've been watching my favourite comedy show for the 100x time: The office, Trailer park boys, South park, and occasionally some anime.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/1ZZ6itkQhcH-j22W4Ak4MzFRPPbAq7Kr4kU21sTEkKb8/edit?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at aymaneigmiden@gmail.com if you have any interesting job opportunities!`,
+  library: `This is my digital library. I have a bunch of games: The Witcher 3, Stardew Valley, Kingdom Come deliverance... and more!
+  I also have some books for the occasional reading!`,
+  projects: `Here are some of my projects.`,
+  exit: `If you want to exit my world, just close the tab.`,
+};
